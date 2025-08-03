704f737339a93fc65d4dee0d5c86ef566c9f4e77
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-03 15:47:39 +0300

    Add PaymentMethod enum for managing payment methods and their attributes

---
+++ app/Enum/Order/PaymentMethod.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Enum\Order;
+
+enum PaymentMethod: string
+{
+    case CREDIT_CARD = 'credit_card';
+    case DEBIT_CARD = 'debit_card';
+    case PHONE_WALLET = 'phone_wallet';
+    case CASH_ON_DELIVERY = 'cash_on_delivery';
+
+
+    public function title(): string
+    {
+        return match ($this) {
+            self::CREDIT_CARD => __('Credit Card'),
+            self::DEBIT_CARD => __('Debit Card'),
+            self::PHONE_WALLET => __('Phone Wallet'),
+            self::CASH_ON_DELIVERY => __('Cash on Delivery'),
+        };
+    }
+
+    public function icon(): string
+    {
+        return match ($this) {
+            self::CREDIT_CARD => '💳',
+            self::DEBIT_CARD => '💳',
+            self::PHONE_WALLET => '📱',
+            self::CASH_ON_DELIVERY => '💵',
+        };
+    }
+
+    public function isOnline(): bool
+    {
+        return match ($this) {
+            self::CREDIT_CARD, self::CASH_ON_DELIVERY, self::PHONE_WALLET => true,
+            self::DEBIT_CARD => false,
+        };
+    }
+
+    public static function options(): array
+    {
+        return collect(self::cases())
+            ->mapWithKeys(fn($item) => [$item->value => $item->title()])
+            ->toArray();
+    }
+}
