6254d6e47d32fafb1bf3b0e97d5fd21c78004786
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-03 15:56:10 +0300

    Change 'status' field type from boolean to string in users table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('is_admin')->default(false);
-            $table->boolean('status')->default('pending_verification');
+            $table->string('status')->default('pending_verification');
             $table->rememberToken();
             $table->timestamps();
         });
