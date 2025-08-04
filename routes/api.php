38d66e4d966c4ed39b486a66a60300d5b886b7a4
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-04 17:19:03 +0300

    Refactor authentication routes for clarity and organization

--- routes/api.php
+++ routes/api.php
@@ -3,28 +3,29 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\AuthController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
+use App\Http\Controllers\Api\Auth\SocialAuthController;
 
 Route::prefix('v1')->group(function () {
 
+    Route::prefix('auth')->group(function () {
 
-      Route::controller(AuthController::class)->prefix('auth')->group(function () {
+        Route::controller(AuthController::class)->group(function () {
+            Route::post('/login', 'login')->name('auth.login')->middleware('throttle:10,1');
+            Route::post('/register', 'register')->name('auth.register')->middleware('throttle:5,1');
 
-        Route::post('/login' , 'login')->name('auth.login')->middleware('throttle:10,1');
-        Route::post('/register' , 'register')->name('auth.register')->middleware('throttle:5,1');
-        Route::post('/logout' , 'logout')->name('auth.logout')->middleware('auth:sanctum');
-        Route::post('/password/reset' , 'resetPassword')->name('auth.password.reset')->middleware('throttle:5,1');
-        Route::middleware('auth:sanctum')->group(function () {
-            Route::post('/verification/resend', 'resendVerification')->name('auth.verification.resend')->middleware('auth:sanctum');
-            Route::post('/verification/verify', 'verifyEmail')->name('auth.verification.verify')->middleware('auth:sanctum');
-            Route::post('/password/reset/verify', 'verifyResetPassword')->name('auth.reset.verify')->middleware(['throttle:5,1', 'auth:sanctum']);
+            Route::post('/verification/resend', 'resendVerificationCode')->name('auth.verification.resend')->middleware('throttle:5,1');
+            Route::post('/verification/verify', 'verifyEmail')->name('auth.verification.verify')->middleware('throttle:5,1');
+            Route::post('/password/reset/send', 'sendResetPasswordCode')->name('auth.password.reset.send')->middleware('throttle:5,1');
+            Route::post('/password/reset', 'resetPassword')->name('auth.password.reset')->middleware('throttle:5,1');
+
+            Route::middleware('auth:sanctum')->group(function () {
+                Route::post('/logout', 'logout')->name('auth.logout');
+            });
         });
 
-      });
-
-
+        Route::prefix('social')->controller(SocialAuthController::class)->group(function () {
+            Route::post('/{provider}', 'socialAuth')
+                ->name('auth.social.auth');
+        });
+    });
 });
