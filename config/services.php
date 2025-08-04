84264eef58826a3f5a064779308cac454b28afbf
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-04 22:00:17 +0300

    Add Google service configuration to services.php

--- config/services.php
+++ config/services.php
@@ -38,6 +38,11 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-    ]
+    ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+    ],
 
 ];
