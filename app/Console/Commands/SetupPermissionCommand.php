46ea23fb1e90f432772e037ba28f7f50af42aec0
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-06 22:20:47 +0300

    Add SetupPermissionCommand to manage application permissions and roles

---
+++ app/Console/Commands/SetupPermissionCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Spatie\Permission\Models\Role;
+use App\Services\PermissionService;
+use Spatie\Permission\Models\Permission;
+
+class SetupPermissionCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'setup:permissions { --fresh : Create fresh permissions }';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Setup permissions for the application';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        if ($this->option('fresh')) {
+            $this->info('Clear Existing Roles and Permissions');
+
+            Permission::query()->delete();
+            $this->info('Existing permissions cleared.');
+
+            Role::query()->delete();
+            $this->info('Existing roles cleared.');
+
+            app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+        }
+
+        PermissionService::createAllPermissions();
+        $this->info('Permissions created successfully.');
+
+        PermissionService::createRoles();
+        $this->info('Roles created successfully.');
+
+
+        $superAdminUser = User::firstOrCreate(['email' => 'admin@example.com'], [
+            'name' => 'Super Admin',
+            'password' => bcrypt('password'),
+        ]);
+
+        if (!$superAdminUser->hasRole('super_admin')) {
+            $superAdminUser->assignRole('super_admin');
+            $this->info('Super admin user created with email: admin@example.com');
+        }
+
+        $this->info('Setup completed successfully.');
+    }
+}
