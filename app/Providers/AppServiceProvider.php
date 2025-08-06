7fbf08eec33bfb531d1b804181d9d21d5685cb76
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-06 22:21:44 +0300

    Register SetupPermissionCommand and implement super admin gate check

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use App\Console\Commands\SetupPermissionCommand;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                SetupPermissionCommand::class,
+            ]);
+        }
+
+        Gate::before(function ($user) {
+            return $user->isSuperAdmin();
+        });
     }
 }
