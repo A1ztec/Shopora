e9701df8e468a314dae80ae1ab84e68850b33b2e
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-23 23:11:19 +0300

    Implement afterCreate method in CreateUser to assign roles and update user status

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -2,11 +2,37 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
-use App\Filament\Resources\UserResource;
 use Filament\Actions;
+use App\Enum\User\UserStatus;
+use Illuminate\Support\Facades\Log;
+use App\Filament\Resources\UserResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function afterCreate(): void
+    {
+        $currentUser = auth()->user();
+
+        if ($currentUser->isSuperAdmin()) {
+            $role = $this->data['role'] ?? 'customer';
+        } else {
+            // Admin can only create customers
+            $role = 'customer';
+        }
+
+        $this->record->assignRole($role);
+        $this->record->update([
+            'email_verified_at' => now(),
+            'status' => UserStatus::ACTIVE
+        ]);
+
+        Log::info('User created', [
+            'id' => $this->record->id,
+            'email_verified_at' => $this->record->email_verified_at,
+            'status' => $this->record->status,
+        ]);
+    }
 }
